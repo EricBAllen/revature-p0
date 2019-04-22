7ce4b53af06b80c009ad06791d2b203aa6127adc
EricBAllen <eric.bartlett.allen@gmail.com>
2019-04-22 13:05:50 -0500

    project0

--- azure-user.sh
+++ azure-user.sh
@@ -19,33 +19,3 @@ cat <<-_EOF_
      $(create_user)
 _EOF_
 
-
- # FREDS CODE BELOW........................................
-#!/bin/bash
-
-
-username=$1
-password=$2
-displayname=$3
-userprincipalname=$displayname@$domain
-DOMAIN=ericallen740yahoo.onmicrosoft.com
-random=${time}
-subscription=$4
-
-## step 1 - login
-az login -u $username -p $password
-
-result=$(az ad user list --query [].userprincipalname | grep -E /$userprincipalname/)
-
-# step 2 create user
-
-if [ -z 'result' ]; then
-
-
-az ad user create --display-name $displayname --user-principal-name $userprincipalname --force-change-password-next-login true --password $random --subscription $users$fi
-
-## step 3 - must be admin
-# az ad user check
-# az ad role -u username
-
-exit 0
